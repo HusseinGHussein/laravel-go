0d6ebb24c413abe608f5652f6d29372abd43eb70
Hussein Hussein <HusseinGHussein@users.noreply.github.com>
2020-12-23 20:33:28 +0200

    Model Observer

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $topic = new \App\Models\Topic;
+    $topic->title = 'Hello there';
+    $topic->save();
 });
